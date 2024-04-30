655cd65c3ac44e41a34e9083d435c67e3539f5ea
Harnassus <micfestus45@gmail.com>
2024-04-30 22:26:33 +0100

    minor fix in mobile nav

--- src/lib/components/MobileNav.svelte
+++ src/lib/components/MobileNav.svelte
@@ -7,32 +7,34 @@
 	};
 </script>
 
-<nav class="fixed bottom-0 z-50 h-28 w-full bg-white shadow shadow-[#00000040] md:hidden">
+<nav class="fixed bottom-0 z-50 h-24 w-full bg-white shadow shadow-[#00000040] md:hidden">
 	<ul class="mx-auto flex h-full w-full max-w-xs items-center justify-between bg-white px-5">
 		<li>
-			<a href="/app" class="flex flex-col items-center justify-center space-y-2">
+			<a href="/app" class="flex flex-col items-center justify-center space-y-1">
 				<img
-					src="/assets/{$page.url.pathname === '/app'
+					src="/assets/{$page.url.pathname === '/app' ||
+					$page.url.pathname.startsWith('/app/course')
 						? 'Menu stroked(active)'
 						: 'Menu stroked'}.svg"
-					class="h-5 w-5"
 					alt="menu icon"
 				/>
-				<span class="text-sm {$page.url.pathname === '/app' && 'text-primary-main-blue'} "
-					>Courses</span
+				<span
+					class="text-sm {($page.url.pathname === '/app' ||
+						$page.url.pathname.startsWith('/app/course')) &&
+						'text-primary-main-blue'} ">Courses</span
 				>
 			</a>
 		</li>
 		<button
-			class="flex h-[73px] w-[73px] items-center justify-center rounded-full border-4 border-white bg-primary-main-green shadow"
+			class="flex h-[73px] w-[73px] items-center justify-center rounded-full border-4 border-white bg-primary-main-green shadow-md"
 			on:click={updateModalState}
 		>
 			<img src="/assets/ic_round-add.svg" alt="add icon" />
 			<span class="sr-only">Add course</span>
 		</button>
 		<li>
-			<a href="/" class="flex flex-col items-center justify-center space-y-2">
-				<img src="/assets/profiled stroked.svg" class="h-5 w-5" alt="menu icon" />
+			<a href="/" class="flex flex-col items-center justify-center space-y-1">
+				<img src="/assets/profiled stroked.svg" alt="menu icon" />
 				<span class="font-lato text-sm">Profile</span>
 			</a>
 		</li>
